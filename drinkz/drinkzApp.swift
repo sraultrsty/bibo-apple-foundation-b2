e2fd6cd783c750c8c309f39cc4be1f43c34fcbc2
Sritrusta Aulia <sritrustaaulia@gmail.com>
2026-07-02 08:52:17 +0700

    Initial Commit

---
+++ drinkz/drinkzApp.swift
@@ -0,0 +1,17 @@
+//
+//  drinkzApp.swift
+//  drinkz
+//
+//  Created by Mac on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct drinkzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ drinkzTests/drinkzTests.swift
@@ -0,0 +1,19 @@
+//
+//  drinkzTests.swift
+//  drinkzTests
+//
+//  Created by Mac on 02/07/26.
+//
+
+import Testing
+@testable import drinkz
+
+struct drinkzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
